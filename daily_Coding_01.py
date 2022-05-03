c09eeeff5ac1a128ca5c75e0c8f5ba5762f74ebb
chanduchukka971 <chanduchukka971@gmail.com>
2022-05-04 01:34:31 +0530

    Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.

--- daily_Coding_01.py
+++ daily_Coding_01.py
@@ -1,16 +1,25 @@
-def checkIfSumIs(k,ls):
+# Given a list of numbers and a number k,
+# return whether any two numbers from the list add up to k.
+
+# For example, given [10, 15, 3, 7] and k of 17,
+# return true since 10 + 7 is 17.
+
+def checkIfSumIs(k, ls):
     for i in range(len(ls)):
         base = ls[0]
         ls.pop(0)
-        n=len(ls)
+        n = len(ls)
         for j in range(n):
             if(ls[j] + base == k):
                 return "true"
-if __name__=="__main__":
-    ls = list(map(int,input("enter the space separated list values:").strip().split()))
+
+
+if __name__ == "__main__":
+    ls = list(map(int, input(
+        "enter the space separated list values:").strip().split()))
     k = int(input("enter the sum value:"))
-    str = checkIfSumIs(k,ls)
-    if(str != None):
+    str = checkIfSumIs(k, ls)
+    if(str is not None):
         print(str)
     else:
         print("false")
--- daily_Coding_02.py
+++ daily_Coding_02.py
@@ -1,17 +1,26 @@
-def productArray(ls,n):
-    left=[0]*n
-    right=[0]*n
-    prod=[0]*n
-    left[0]=1
-    right[n-1]=1
-    for i in range(1,n):
-        left[i]=ls[i-1]*left[i-1]
-    for j in range(n-2,-1,-1):
-        right[j]=ls[j+1]*right[j+1]
+# Given an array of integers, return a new array such that each element
+# at index i of the new array is the product of all the numbers in the
+# original array except the one at i.
+
+# For example, if our input was [1, 2, 3, 4, 5],
+# the expected output would be [120, 60, 40, 30, 24].
+# If our input was [3, 2, 1], the expected output would be [2, 3, 6].
+def productArray(ls, n):
+    left = [0]*n
+    right = [0]*n
+    prod = [0]*n
+    left[0] = 1
+    right[n-1] = 1
+    for i in range(1, n):
+        left[i] = ls[i-1]*left[i-1]
+    for j in range(n-2, -1, -1):
+        right[j] = ls[j+1]*right[j+1]
     for i in range(n):
-        prod[i]=left[i]*right[i]
+        prod[i] = left[i]*right[i]
     print(prod)
-ls=list(map(int,input().strip().split()))
-n=len(ls)
+
+
+ls = list(map(int, input().strip().split()))
+n = len(ls)
 print("the product array is:")
-productArray(ls,n)
+productArray(ls, n)
